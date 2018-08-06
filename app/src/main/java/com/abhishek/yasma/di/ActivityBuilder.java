4e0462ab5dcef7d31c445de91757f3fa4dcf388b
Abhishek Tiwari <abhitiwari472@gmail.com>
2018-08-06 18:22:14 +0530

    Add MainActivity Injector in ActivityBuilder and injecting ViewModelProvider.Factory in MainActivity

--- app/src/main/java/com/abhishek/yasma/di/ActivityBuilder.java
+++ app/src/main/java/com/abhishek/yasma/di/ActivityBuilder.java
@@ -1,8 +1,14 @@
 package com.abhishek.yasma.di;
 
+import com.abhishek.yasma.ui.MainActivity;
+
 import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
 
 @Module
 abstract class ActivityBuilder {
+
+    @ContributesAndroidInjector
+    abstract MainActivity contributeMainInjector();
 
 }
--- app/src/main/java/com/abhishek/yasma/ui/MainActivity.java
+++ app/src/main/java/com/abhishek/yasma/ui/MainActivity.java
@@ -1,15 +1,29 @@
 package com.abhishek.yasma.ui;
 
+import android.arch.lifecycle.ViewModelProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.abhishek.yasma.R;
 
-public class MainActivity extends AppCompatActivity {
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
+import dagger.android.support.DaggerAppCompatActivity;
+
+public class MainActivity extends DaggerAppCompatActivity {
+
+    @Inject
+    ViewModelProvider.Factory factory;
+
+    MainActivityViewModel viewModel;
 
     @Override
      public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AndroidInjection.inject(this);
+
+
     }
 }
