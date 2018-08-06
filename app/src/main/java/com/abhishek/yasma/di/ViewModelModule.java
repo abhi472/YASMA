b3861e724cadfd1b66287b0f383bf52c5960f69a
Abhishek Tiwari <abhitiwari472@gmail.com>
2018-08-06 18:11:30 +0530

    Add MainActivityViewModel and it's dependencies provided

--- app/src/main/java/com/abhishek/yasma/di/ViewModelModule.java
+++ app/src/main/java/com/abhishek/yasma/di/ViewModelModule.java
@@ -1,7 +1,23 @@
 package com.abhishek.yasma.di;
 
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+
+import com.abhishek.yasma.ui.MainActivityViewModel;
+
+import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.IntoMap;
 
 @Module
 interface ViewModelModule {
+
+    @Binds
+    ViewModelProvider.Factory bindViewModelFactory(ViewModelFactory factory);
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainActivityViewModel.class)
+    ViewModel bindMainViewModel(MainActivityViewModel viewModel);
+
 }
---
+++ app/src/main/java/com/abhishek/yasma/ui/MainActivityViewModel.java
@@ -0,0 +1,13 @@
+package com.abhishek.yasma.ui;
+
+import com.abhishek.yasma.base.BaseViewModel;
+
+import javax.inject.Inject;
+
+public class MainActivityViewModel extends BaseViewModel<Object> {
+
+    @Inject
+    MainActivityViewModel() {
+
+    }
+}
